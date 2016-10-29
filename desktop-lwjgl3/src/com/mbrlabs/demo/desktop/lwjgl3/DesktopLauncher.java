99f50255a827bdf3c5b56a32c14a2e1c8d5a8207
Marcus Brummer <mbrlabs7@gmail.com>
2016-10-29 14:27:50 +0200

    Initial commit

---
+++ desktop-lwjgl3/src/com/mbrlabs/demo/desktop/lwjgl3/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mbrlabs.demo.desktop.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.mbrlabs.demo.MundusDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new MundusDemo(), config);
+	}
+}
---
+++ desktop/src/com/mbrlabs/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mbrlabs.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mbrlabs.demo.MundusDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MundusDemo(), config);
+	}
+}
